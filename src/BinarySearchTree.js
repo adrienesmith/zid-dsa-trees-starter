341b362b112c8c4465c9a122cae29cb2c4fdb345
Adriene Smith <adriene.e.smith@gmail.com>
2021-07-07 09:11:38 -0700

    added depth-first search

--- src/BinarySearchTree.js
+++ src/BinarySearchTree.js
@@ -121,4 +121,32 @@ class BinarySearchTree {
     }
     return this.left._findMin()
   }
+
+  dfsInOrder(values = []) {
+    // First, process the left node recursively
+    if (this.left) {
+      values = this.left.dfsInOrder(values);
+    }
+    // Next, process the current node
+    values.push(this.value);
+    // Finally, process the right node recursively
+    if (this.right) {
+      values = this.right.dfsInOrder(values);
+    }
+    return values;
+  }
+
+  dfsPreOrder(values = []) {
+    // First, process the current node
+    values.push(this.value);
+    // Next, process the left node recursively
+    if (this.left) {
+      values = this.left.dfsPreOrder(values);
+    }
+    // Finally, process the right node recursively
+    if (this.right) {
+      values = this.right.dfsPreOrder(values);
+    }
+    return values;
+  }
 }
