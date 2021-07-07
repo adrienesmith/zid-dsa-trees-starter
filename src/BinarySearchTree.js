e41283d82d248f0a2612668c9ff5be636610b73e
Adriene Smith <adriene.e.smith@gmail.com>
2021-07-07 07:47:52 -0700

    completed assignment

--- src/BinarySearchTree.js
+++ src/BinarySearchTree.js
@@ -1,3 +1,124 @@
 class BinarySearchTree {
-  // your code here
+  constructor(key = null, value = null, parent = null) {
+    this.key = key;
+    this.value = value;
+    this.parent = parent;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(key, value) {
+    // if tree is empty, the node getting inserted becomes the root
+    if (this.key == null) {
+      this.key = key;
+      this.value = value;
+    }
+    // if the tree already exists, compare the root to they key being inserted
+    // if the new key < the node key, the new node goes on the left
+    else if (key < this.key) {
+      // if the existing node doesn't have a left child
+      // the node being inserted becomes the start of a new sub-tree
+      if (this.left == null) {
+        this.left = new BinarySearchTree(key, value, this);
+      // if the existing node does have a left child
+      // recursively call the insert() method
+      } else {
+        this.left.insert(key, value);
+      }
+    // do the same comparisons for the right side
+    // but for if they node being inserted is greater than the root node
+    } else {
+      if (this.right == null) {
+        this.right = new BinarySearchTree(key, value, this);
+      } else {
+        this.right.insert(key, value);
+      }
+    }
+  }
+
+  find(key) {
+    // if found at root, return root value
+    if (this.key == key) {
+      return this.value;
+    }
+    // if less than root, and left child exists, recursively search the left side
+    else if (key < this.key && this.left) {
+      return this.left.find(key);
+    }
+    // if greater than root, and right child exists, recursively search the right side
+    else if (key > this.key && this.right) {
+      return this.right.find(key);
+    }
+    // search complete, throw error if not found
+    else {
+      throw new Error('Key Not Found'); 
+    }
+  }
+
+  remove(key) {
+    if (this.key == key) {
+      if (this.left && this.right) {
+        const successor = this.right._findMin();
+        this.key = successor.key;
+        this.value = successor.value;
+        successor.remove(successor.key);
+      }
+      // if the node only has a left child, replace the node with its left child
+      else if (this.left) {
+        this._replaceWith(this.left);
+      }
+      // if the node only has a right child, replace the node with its right child
+      else if (this.right) {
+        this._replaceWith(this.right);
+      }
+      // if the node has no children, remove it and any references to it
+      else {
+        this._replaceWith(null);
+      }
+    }
+    else if (key < this.key && this.left) {
+      this.left.remove(key);
+    }
+    else if (key > this.key && this.right) {
+      this.right.remove(key);
+    }
+    else{
+      throw new Error('Key Not Found');
+    }
+  }
+
+  _replaceWith(node) {
+    if (this.parent) {
+      if (this == this.parent.left) {
+        this.parent.left = node;
+      }
+      else if (this == this.parent.right) {
+        this.parent.right = node;
+      }
+      if (node) {
+        node.parent = this.parent;
+      }
+    }
+    else {
+      if (node) {
+        this.key = node.key;
+        this.value = node.value;
+        this.left = node.left;
+        this.right = node.right;
+      }
+      else {
+        this.key = null;
+        this.value = null;
+        this.left = null;
+        this.right = null;
+      }
+    }
+  }
+
+  _findMin() {
+    if (!this.left) {
+      return this;
+    }
+    return this.left._findMin()
+  }
 }
